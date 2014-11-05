9f12c299bbe8e4b983746e9789a01b9f6fce9379
Mazur93 <bartoszmazurkiewicz899@gmail.com>
2014-11-05 13:41:02 +0100

    Der Button zum Erstellen eines neuen Topics ist oben. Danach klickt man in die Karte und es erscheint ein Marker mit Label. Am Pop-up-Fenster fuer die Eingaben(URL,Titel,...) arbeite ich noch

---
+++ js/leaflet-Interaktion/Karteninteraktion.js
@@ -0,0 +1,26 @@
+/**
+ * @author Mazur93
+ */
+function newMarker(){
+	addMarker = true;
+	};
+		
+function resetView(){
+	map.setView([30.505, 0.000], 2);
+	}
+	
+function onMapClick(e) {
+			if(addMarker == true){
+			
+			L.marker(e.latlng).addTo(map).bindPopup("Titel: <br />Katastrophenart: <br />URL: <br />Bewertung: <br />Breitengrad: " 
+													+ e.latlng.lat + "<br />Längengrad: " + e.latlng.lng + "<br /><br /><a>Mehr Infos...</a>"
+													) ;
+			
+			addMarker=false;
+		}
+		}
+		
+
+		
+
+
