292083292301af010ee8a0adae0306a6ead1c1d4
joanago <j_gock02@uni-muenster.de>
2014-11-18 11:49:08 +0100

    Variablen für Formular zur Datenübertragung in DB

--- de/ErsterVersuch.php
+++ de/ErsterVersuch.php
@@ -10,12 +10,45 @@ $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
 
 $result = pg_query($con, "INSERT INTO test(spalte1, spalte2)
 					VALUES('asdf', 'jfghj');");
+					
+	$url = $_POST['URL'];
+	$titel = $_POST['Titel'];
+	$kommentar = $_POST['Kommentar'];
+	
+    $kategorie = $_POST['Kategorie'];
+	$katwert = NULL;
+    switch ($klasse) { 
+        case 'Welt': 
+            $katwert = 'Welt';
+            break; 
+        case 'Kontinent': 
+            $katwert = 'Kontinent';
+            break; 
+        case 'Land': 
+            $katwert = 'Land';
+            break; 
+        case 'Region': 
+            $katwert = 'Region';
+            break; 
+        case 'Stadt': 
+            $katwert = 'Stadt';
+            break; 	
+        default:
+			$katwert = NULL;
+            echo 'NULL<br />';
+    }
+	
+	$start = $_POST['star'];
+	$end = $_POST['end'];
+	$bewertung = $_POST['Bewertung'];
+	$tags = $_POST['Tags'];
+	$hyperlink = $_POST['Hyperlink'];
 
 
-//dump the reuslut object
-var_dump($result);
+	//dump the result object
+	var_dump($result);
 
-//closing connection
-pg_close($con); 
+	//closing connection
+	pg_close($con); 
 
 ?>
