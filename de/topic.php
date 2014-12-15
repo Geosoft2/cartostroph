7a90c73b8a7b0c405d6ef49a5549afd3c344b971
PhilippStuewe <philipp.stuewe@uni-muenster.de>
2014-12-15 10:57:48 +0100

    Update topic.php
    einfügen der position funktioniert, bewertung noch nicht richtig

--- de/topic.php
+++ de/topic.php
@@ -12,7 +12,7 @@
 	$kommentar = $_POST['Kommentar'];
 	$breitengrad = $_POST['Breitengrad'];
 	$laengengrad = $_POST['Längengrad'];
-	$position = '$breitengrad, $laengengrad';
+	//$position = '$breitengrad, $laengengrad';
 	$autor = $_POST['Autor'];
 
     $kategorie = $_POST['Kategorie'];
@@ -40,14 +40,19 @@
 			$katwert = NULL;
     }
 	
-	$start = $_POST['start'];
+   	$start = $_POST['start'];
 	$end = $_POST['end'];
-	$bewertung = $_POST['sliderOutput3'];
+	//$start = date('$start', 'DD Mon YYYY');   //to_date('05 Dec 2000', 'DD Mon YYYY')
+	//$end = date('$end', 'DD Mon YYYY');
+	$bewertung = (int) $_POST['Bewertung'];
 	$tags = $_POST['tags'];
 	$hyperlink = $_POST['hyperlink'];
+	$autor = 'Anonym';
+
+	
 	
 	$result = pg_query($connection, "INSERT INTO topic(url_top, text, bewertung, hyperlink, anfangsdatum, enddatum, kategorie, titel, position, autor, Tag) 
-					VALUES('$url', '$kommentar', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '$autor', NULL)");
+					VALUES('$url', '$kommentar', $bewertung, '$hyperlink', Null, Null, '$katwert', '$titel', Point($breitengrad, $laengengrad), '$autor', '$tags')");
 					//VALUES('$url', '$kommentar', $bewertung, '$hyperlink', $start, $end, '$katwert', '$titel', $position, '$autor', '$tags')");
 
 	//header("Location: index.html");
