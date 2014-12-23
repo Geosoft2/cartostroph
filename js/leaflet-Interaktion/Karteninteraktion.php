cd9a8fb264a26cab93ca1437a2323d2eacc7ae6a
joanago <j_gock02@uni-muenster.de>
2014-12-23 15:56:37 +0100

    Marker hinzufügen

--- js/leaflet-Interaktion/Karteninteraktion.php
+++ js/leaflet-Interaktion/Karteninteraktion.php
@@ -1,6 +1,5 @@
 <?php
 	header(’Content-Type: text/javascript’);
-	include("layer.php");
 ?>
 <script type="text/javascript"> 
 
@@ -42,6 +41,24 @@ function submitTopic(){
 	var Autor = autor();
 	var Position;
     <?php    
+	// attempt a connection
+	ini_set('display_errors', '1');
+	error_reporting(E_ALL | E_STRICT);
+
+	include("config.php");
+	global $config;
+	$connection = pg_connect($config["connection"]);
+	if (!$connection) {
+		die("Error in connection: " . pg_last_error());
+	}
+
+	// execute query
+	$sql = "SELECT url_top, titel, position, bewertung, autor FROM topic";
+	$result = pg_query($connection, $sql);
+	if (!$result) {
+		die("Error in SQL query: " . pg_last_error());
+	}
+	
 	// iterate over result set
 	// print each row
 	while ($row = pg_fetch_array($result)) {
@@ -56,16 +73,20 @@ function submitTopic(){
 		var Titel = $Titel;
 		var Bewertung = $Bewertung;
 		var Autor = $Autor;
-		var Position = $Position;';
+		var Position = $Position;
+		
+		L.marker([lat, lng]).addTo(map).bindPopup("Titel: " + Titel + "<br />Bewertung: "
+								       		 + Bewertung + "<br />Breitengrad: " + "<br/> Autor: " + Autor  + "<br /><br /><a>Mehr Infos...</a>")';
 	}
-    
+	pg_free_result($result);
        
-    ?>           
-	L.marker([lat, lng]).addTo(map).bindPopup("Titel: " + Titel + "<br />Bewertung: "
-								       		 + Bewertung + "<br />Breitengrad: " + "<br/> Autor: " + Autor  + "<br /><br /><a>Mehr Infos...</a>");	
+    ?>   
+	
+		
 	addMarker = false;
 	document.getElementById("map").removeAttribute("data-reveal-id");				       		 								
 }
+
 
 function activateAssessment() {
 	 if(document.getElementById("checkbox1").checked == false){
