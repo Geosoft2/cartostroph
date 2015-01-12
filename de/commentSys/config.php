031f07b5ec67f0d4c7139d869ac3cc9a94d0f9b4
Sijoma <simonschulte1991@gmail.com>
2015-01-12 15:10:05 +0100

    Comment System
    Anfang vom Comment System, roh Funktionalität

---
+++ de/commentSys/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	$config["host"] = "localhost";
+ 
+	$config["dbname"] = "postgres";
+ 
+	$config["user"] = "j_gock02";
+ 
+	$config["password"] = "Joana12";
+	
+	$config["connection"] = "host=localhost dbname=postgres user=j_gock02 password=Joana123";
+	
+ ?>
