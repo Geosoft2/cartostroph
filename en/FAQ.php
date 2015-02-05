83878cff2b6d08851cf92566a8f24100d00f1e38
joanago <j_gock02@uni-muenster.de>
2015-02-05 01:26:35 +0100

    Englisch Login

--- en/FAQ.php
+++ en/FAQ.php
@@ -23,6 +23,18 @@
             <section class="top-bar-section">
                 <!-- Right Nav Section -->
                 <ul class="right">
+					<ul class="right">
+                	<li>
+                    	</a> <a href="../de"><img src="../img/germany.gif"></a>
+                    </li>
+                    
+                    <li>
+                    	</a> <a href="../en"><img src="../img/uk.gif"></a>
+                    </li>
+                    
+                    <li>
+                    	</a> <a href="../pl"><img src="../img/poland.gif"></a>
+                    </li>
 
 
                     <!-- Loginfunktion -->
--- en/login2.php
+++ en/login2.php
@@ -25,7 +25,18 @@
 
                 <!-- Right Nav Section -->
                 <ul class="right">
-                    <li><a href="FAQ.php">Hilfe</a></li>
+                	<li>
+                    	</a> <a href="../de"><img src="../img/germany.gif"></a>
+                    </li>
+                    
+                    <li>
+                    	</a> <a href="../en"><img src="../img/uk.gif"></a>
+                    </li>
+                    
+                    <li>
+                    	</a> <a href="../pl"><img src="../img/poland.gif"></a>
+                    </li>
+                    <li><a href="FAQ.php">Help</a></li>
 
                  <!-- Loginfunktion -->
                     <li class="has-dropdown">
@@ -34,7 +45,7 @@
 					
 				 <!-- Pop-Up für Registrierung  -->
                     <li>
-                    	<a id="regis" href="#" data-reveal-id="RegisterModal">Registrierung</a>
+                    	<a id="regis" href="#" data-reveal-id="RegisterModal">Registration</a>
                     	<script type="text/javascript">
                     		if (loggedIn() == "Login"){
 					
@@ -50,19 +61,19 @@
 					<li class="has-form">
 						<div class="row collapse">
 							<form action="filter.php" method="get">
-								<input type="text" placeholder="Schnellsuche" name="search">
+								<input type="text" placeholder="Quick search" name="search">
 							</form>
 						</div>
 					</li>
 					
                     <!-- Suchfeld -->
 					 <li>
-						<a href="search.php">Suche</a>
+						<a href="search.php">Search</a>
                     </li>
 					
 					<!-- Impressum aufrufen -->
                     <li>
-                        <a href="Impressum.php">Impressum</a>
+                        <a href="Impressum.php">Imprint</a>
                     </li>
 					
                 </ul>
@@ -72,62 +83,132 @@
 
 	<!-- PopUp-Registrierungs-Formular -->
     <div id="RegisterModal" class="reveal-modal" data-reveal>
-        <h2> Registrierung </h2>
+        <h1> Registration </h1>
         <form action="register.php" method="post">
-            Benutzername: <input type="text" id="Benutzername" name="Benutzername" required />
-            Passwort: <input type="password" id="passwort" name="Passwort" required />
-            Passwort wiederholen:<input type="password" id="passwortWieder" name="Passwort2" required />
-            Ort (optional): <input type="text" name="Ort" id="Ort" />
-            PLZ (optional): <input type="text" name="PLZ" id="PLZ" />
-            Land (optional): <input type="text" name="Land" id="Land" />
+            Username: <input type="text" id="Benutzername" name="Benutzername" required />
+            Password: <input type="password" id="passwort" name="Passwort" required />
+            Repeat password:<input type="password" id="passwortWieder" name="Passwort" required />
+            Location (optionally): <input type="text" name="Ort" id="Ort" />
+            Postcode (optionally): <input type="text" name="PLZ" id="PLZ" />
+            Country (optionally): <input type="text" name="Land" id="Land" />
             <input id="regist" type="submit" class="button expand" value="Registrieren" />
-            <a style="text-align: right ;position: relative ; font-size: 120%" class="close-reveal-modal">Abbrechen</a><br />
-            <br />
+            <a style="text-align: right ;position: relative ; font-size: 120%" class="close-reveal-modal">Abbrechen</a>
         </form>
     </div>
+	
 
     <!-- Inhalt -->
    <div id="registercontent">
        </br>
-	<p>Ihr Login schlug fehl.</p>
-		<p>Bitte überprüfen Sie Ihr Passwort und Ihren Benutzernamen und versuchen es erneut.</p>
-		<p>Wenn sie Ihr Passwort oder Ihren Benutzernamen vergessen haben registrieren Sie sich bitte neu.</p>
+	<p>Your Login failed.</p>
+		<p>Please check your password and username and try again.</p>
+		<p>When you forgot your password or username, please make a new registration.</p>
 		 <form id="top-nav-login" action="login.php" method="post">
             <div class="row">
-                <label>Nutzer</label>
+                <label>User</label>
                 <input type="text" name="user" placeholder="name" tabindex="1" />
             </div>
             <div class="row">
-                <label>Passwort</label>
+                <label>Password</label>
                 <input type="password" name="password" placeholder="********" tabindex="2" />
             </div>
             <div class="row">
                 <input type="submit" class="button tiny success" value="Login" tabindex="3" />
             </div>
-            <p>Sie haben noch kein Konto? Zur Registrierung geht es <a onclick="test" data-reveal-id="RegisterModal">hier</a></p>
+            <p>You don't have a registry? Please register <a onclick="test" data-reveal-id="RegisterModal">here</a></p>
         </form>
    </div>
 
 
-    <!-- Dropdown-Login-Feld -->
-    <div id="login-dropdown" class="f-dropdown small content" data-dropdown-content="true" width="10%">
+   <!-- Dropdown-Login-Feld -->
+   <div id="login-dropdown" class="f-dropdown small content" data-dropdown-content="true" width="10%">
         <h5>Log In:</h5>
 		
         <form id="top-nav-login" action="login.php" method="post">
             <div class="row">
-                <label>Nutzer</label>
+                <label>User</label>
                 <input type="text" name="user" placeholder="email@example.com" tabindex="1" />
             </div>
             <div class="row">
-                <label>Passwort</label>
-                <input type="password" name="password" placeholder="********" tabindex="2" />
+                <label>Password</label>
+                <input type="password" placeholder="********" tabindex="2" />
             </div>
             <div class="row">
                 <input type="submit" class="button tiny success" value="Login" tabindex="3" />
             </div>
-            <p>Sie haben noch kein Konto? Zur Registrierung geht es <a onclick="test" data-reveal-id="RegisterModal">hier</a></p>
+            <p>You don't have a login? Click <a onclick="test" data-reveal-id="RegisterModal">here</a> for registration.</p>
         </form>
     </div>
+	
+	<!-- Dropdown-Eingeloggt-Feld -->
+	<div id="loggedin-dropdown" class="f-dropdown small content" data-dropdown-content="true" width="10%">
+		<h5 id="eingeloggtAls"><h5>
+			<script>
+				document.getElementById("eingeloggtAls").innerHTML = "Eingeloggt als: " + author();
+			</script>
+			<ul id="drop" class="[tiny small medium large content]f-dropdown" data-dropdown-content>
+			  <a href="#" data-reveal-id="Profile">Profile</a>
+
+					<div id="Profile" class="reveal-modal" data-reveal>
+					  <h3 id="benutzername">My profile: </h3>
+					  
+					  <script>
+						document.getElementById("benutzername").innerHTML = "Mein Profil: " + author();
+					  </script>
+					  <form action="alteruser.php" method="post">
+					  <button style="float: right;">change data</button>
+						<?php
+						// attempt a connection
+						//ini_set('display_errors', '1');
+						//error_reporting(E_ALL | E_STRICT);
+						include("config.php");
+						global $config;
+
+						$connection = pg_connect($config["connection"]);
+						if (!$connection) {
+							die("Error in connection: " . pg_last_error());
+						}
+						
+						$user = $_COOKIE['Autor'];
+
+						// execute query
+						$sql = "SELECT ort, plz, land FROM nutzer WHERE name='$user';";
+	
+						$result = pg_query($connection, $sql);
+						if (!$result) {
+							die("Error in SQL query: " . pg_last_error());
+						}
+
+						// iterate over result set
+						// print each row
+						while ($row = pg_fetch_array($result)) {
+							$ort = (string)$row[0];
+							$plz = (string)$row[1];
+							$land = (string)$row[2];
+						
+							echo '<p>Location: ' . $ort . ' </p>';
+							echo '<p>Postcode: ' . $plz . '</p>';
+							echo '<p>Country: ' . $land . '</p>';
+							//echo '<p>Ort: <input value=' . $ort . ' type=\"text\" id=\"ort\" name=\"ort\" style=\"width: 90%;\"/></p>';
+							//echo '<p>PLZ: <input value=' . $plz . ' type=\"text\" id=\"plz\" name=\"plz\" style=\"width: 90%;\"/></p>';
+							//echo '<p>Land: <input value=' . $land . ' type=\"text\" id=\"land\" name=\"land\" style=\"width: 90%;\"/></p>';
+						}
+
+						// free memory
+						pg_free_result($result);
+						?>
+						<input value="ort" type="text" id="ort" name="ort" style="width: 90%;"/>
+						<input value="plz" type="text" id="plz" name="plz" style="width: 90%;"/>
+						<input value="land" type="text" id="land" name="land" style="width: 90%;"/>
+						<a class="close-reveal-modal">&#215;</a>
+						</form>
+					</div>
+			
+			  
+			  <br /><a href="infouser.php">My Topics and Comments</a>
+			  <br /><a href="logout.php">Logout</a>
+			</ul>
+	</div>
 	
 	
 	<script src="../js/vendor/jquery.js"></script>
